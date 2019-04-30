70fd3ac46ba290560dff6a671ca356acc5738013
Toby Evans <toby.evans01@bbc.co.uk>
2019-04-30 14:57:18 +0100

    added POST

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import pandas as pd
+import json
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -8,7 +9,16 @@ app.config["DEBUG"] = True
 def hello():
     return "I haz mad hacker skillz!"
 
-@app.route("/api")
+@app.route("/api", methods = ["GET", "POST"])
 def api():
-	data = pd.DataFrame({'words':['one', 'two','three']})
-	return data.to_json(orient='records')
+	if request.method == "GET":
+		print("I iz getting data")
+		data = pd.DataFrame({'words':['one', 'two','three']})
+		return data.to_json(orient='records')
+
+	print("I iz posting data")
+	print(request.get_json(force=True))
+	print(request.form)
+	return jsonify(request.get_json(force=True))
+
+
