c9b8238fa2f438184921aede1a50d5b2f66bfef0
Toby Evans <toby.evans01@bbc.co.uk>
2019-04-30 12:39:36 +0100

    api

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,14 @@
 from flask import Flask, jsonify
+import pandas as pd
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route("/")
 def hello():
-    return "I haz mad hacker skillzzzzz!"
+    return "I haz mad hacker skillz!"
+
+@app.route("/api")
+def api():
+	data = pd.DataFrame({'words':['one', 'two','three']})
+	return data.to_json(orient='records')
